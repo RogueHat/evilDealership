57550da046edcc3d5b46ef167b6ee3dfa25ab626
736173 <736173@RH-TS-HP8000-09.district196.org>
2012-10-31 13:09:09 -0500

    more methods

--- EvilCarDealership/src/Car.java
+++ EvilCarDealership/src/Car.java
@@ -1,12 +1,42 @@
 
 public class Car {
-	int leftBound = 75;
-	int rightBound = 275;
-	int topBound = 60;
-	int botBound = 260;
+	int leftBound = -100;
+	int rightBound = 100;
+	int topBound = 100;
+	int botBound = -100;
+	double charge;
+	int speed;
 	int x;
 	int y;
+	public Car() {
+		x = 0;
+		y =0;
+		speed = 0;
+		charge = .99;
+	}
 	public void isInside() {
-		if(this)
+		if(x<leftBound || x > rightBound || y < botBound || y > topBound)
+			charge += 10000;
+	}
+	public int getx() {
+		return x;
+	}
+	public void setx(int X) {
+		x=X;
+	}
+	public int gety() {
+		return y;
+	}
+	public void sety(int Y) {
+		y=Y;
+	}
+	public int getSpeed() {
+		
+	}
+	public double getcharge() {
+		return charge;
+	}
+	public void setCharge(double j) {
+		charge = j;
 	}
 }
