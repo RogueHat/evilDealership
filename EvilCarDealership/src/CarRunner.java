4b1c4e975250c0a04a982c6212001afe43570bb8
CarloSchool <carlomiguelh@yahoo.com>
2012-11-01 13:21:22 -0500

    adfs

---
+++ EvilCarDealership/src/CarRunner.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class CarRunner{
+	public static void main(String[]args) throws IOException{
+		Scanner listen;
+		for(int t = 0;;t++){
+			listen = new Scanner(new File("car.txt"));
+			
+		}
+		
+	}
+}
