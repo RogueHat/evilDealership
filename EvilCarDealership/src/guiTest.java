ecf980832db2f828a00948008fc4129f7da0d0f3
CarloSchool <carlomiguelh@yahoo.com>
2012-11-05 13:23:37 -0600

    Carlo Commit

--- EvilCarDealership/src/guiTest.java
+++ EvilCarDealership/src/guiTest.java
@@ -1,4 +1,15 @@
-
-public class guiTest {
-
+import javax.swing.*;
+public class guiTest extends JFrame{
+	JFrame frame;
+	JPanel panel;
+	JTextField field;
+	
+	public guiTest(){
+		super("GUI test");
+		frame = this;
+		panel = new JPanel();
+		field = new JTextField("Hello Biotches",);
+		
+		
+	}
 }
