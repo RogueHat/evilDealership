52ea22e0178052360b0e4c7ad7c26dac575255f0
CarloSchool <carlomiguelh@yahoo.com>
2012-11-06 13:25:04 -0600

    11/6/12

--- EvilCarDealership/src/guiTest.java
+++ EvilCarDealership/src/guiTest.java
@@ -1,15 +1,23 @@
 import javax.swing.*;
-public class guiTest extends JFrame{
+
+public class guiTest extends JFrame {
 	JFrame frame;
 	JPanel panel;
 	JTextField field;
-	
-	public guiTest(){
+
+	public guiTest() {
 		super("GUI test");
 		frame = this;
 		panel = new JPanel();
-		field = new JTextField("Hello Biotches",);
-		
-		
+		field = new JTextField("Hello Biotches", 10);
+		frame.add(panel);
+		panel.add(field);
+	}
+
+	public static void main(String[] args) {
+		guiTest window = new guiTest();
+		window.pack();
+		window.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		window.setVisible(true);
 	}
 }
